3ca464778ed18018d75121f8cf1e36ea063e7b17
Ayca Nur Uysal <uysalayc@gmail.com>
2026-03-24 11:13:15 +0300

    ilk ödev

---
+++ src/Giris/NotOrtalamasiHesapla.java
@@ -0,0 +1,36 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class NotOrtalamasiHesapla {
+    public static void main(String[] args) {
+
+        int mat,fizik,kimya,turkce,tarih,muzik;
+
+        Scanner inp =new Scanner(System.in);
+
+        System.out.print("Matematik Notunuz : ");
+        mat =inp.nextInt();
+
+        System.out.print("Fizik Notunuz : ");
+        fizik =inp.nextInt();
+
+        System.out.print("Kimya Notunuz : ");
+        kimya =inp.nextInt();
+
+        System.out.print("Türkçe Notunuz : ");
+        turkce =inp.nextInt();
+
+        System.out.print("Tarih Notunuz : ");
+        tarih =inp.nextInt();
+
+        System.out.print("Müzik Notunuz : ");
+        muzik =inp.nextInt();
+
+        int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
+        double sonuc = toplam / 6.0;
+
+        System.out.println("Ortalamanız : " + sonuc);
+        System.out.println(sonuc>50 ? "Geçti": "Kaldı");
+    }
+}
